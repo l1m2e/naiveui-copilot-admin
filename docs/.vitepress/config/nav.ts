50c105e8446fb89fccd92ed1cd168a060be0af51
l1m2e <1956227985@qq.com>
2025-11-28 13:54:54 +0800

    feat 移除 playground

--- docs/.vitepress/config/nav.ts
+++ docs/.vitepress/config/nav.ts
@@ -5,5 +5,4 @@ export const nav: DefaultTheme.NavItem[] = [
   { text: '指南', link: '/guide/getting-started' },
   { text: '组件', link: '/components/overview' },
   { text: '组合式函数', link: '/composables/use-form' },
-  { text: 'Playground', link: '/playground' },
 ]
