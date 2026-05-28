ebab9e8e232b0461413be49b572642e8a24a4e21
limingze <lmz@e-nutrition.com.cn>
2026-05-28 10:35:49 +0800

    feat: 添加主题覆盖配置到 n-config-provider

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+import type { GlobalThemeOverrides } from 'naive-ui'
 import { dateZhCN, zhCN } from 'naive-ui'
+
+const themeOverrides: GlobalThemeOverrides = {
+  common: {
+    borderRadius: '8px',
+    borderRadiusSmall: '8px',
+  },
+}
 </script>
 
 <template>
-  <n-config-provider :date-locale="dateZhCN" :locale="zhCN">
+  <n-config-provider :date-locale="dateZhCN" :locale="zhCN" :theme-overrides="themeOverrides">
     <n-message-provider>
       <main font-sans>
         <router-view />
